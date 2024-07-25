7f8e57f4968d8e35fc7be45694df4b23710c9d9b
Oran High Tech <43454838+OraHighTech@users.noreply.github.com>
2024-07-25 13:02:44 +0100

    Add files via upload

---
+++ indicatif.js
@@ -0,0 +1,8 @@
+ // Object containing country codes
+     const countryCodes = [
+            { name: 'France', code: '+33' },
+            { name: 'USA', code: '+1' },
+            { name: 'UK', code: '+44' },
+            { name: 'Germany', code: '+49' }
+            // Add more countries as needed
+        ];
